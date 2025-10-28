6badc7d2529986cdd3d13bb516817cc95a13fb75
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2025-10-28 13:03:26 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ function App() {
 
   return (
     <>
-      <h1 className='flex justify-center align-center font-black text-[12px] sm:text-[28px] lg:text-[36px] xl:text-[40px]'>Estevam 3D Designer</h1>
-    </>
+    <div className='flex justify-center align-center w-full h-screen bg-gradient-to-b from-black via-gray-800 to-gray-900'>
+      <h1 className='flex justify-center align-center font-black text-[18px] sm:text-[28px] lg:text-[36px] xl:text-[40px] text-white mt-18'>Estevam 3D Designer</h1>
+    </div>
+  </>
   )
 }
 
