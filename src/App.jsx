fb939a5f03d1e695896e118dd693df86600ca8b2
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2025-10-28 12:58:43 -0300

    Create Site

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='flex justify-center align-center font-black text-[12px] sm:text-[28px] lg:text-[36px] xl:text-[40px]'>Estevam 3D Designer</h1>
+    </>
+  )
+}
+
+export default App
