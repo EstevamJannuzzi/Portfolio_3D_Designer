521d0c8c2a6217a54b9efaccd4ca94804d578dbc
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2025-10-28 16:37:27 -0300

    save

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Menu from '../assets/Components/Menu'
+import Menu from '../assets/Components/Menu.jsx'
 
 const home = () => {
     return (
         <>
             <Menu />
+            <hr className='border-green' />
             <div className='flex justify-center align-center border-0 rounded-[12px] bg-white/25 p-4 m-4 w-[320px] sm:w-[410px] lg:w-[390px] xl:w-[475px]'>
                 <h1 className='text-primary font-bold text-lg'>Home</h1>
             </div>
--- src/assets/Components/Menu.jsx
+++ src/assets/Components/Menu.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Banners from '../images/Banners.png'
+import Logo from '../images/Logo3D.png'
 
 const menu = () => {
   return (
-    <div className='flex flex-inline'>
-      <h1>Home</h1>
-      <h2>Menu</h2>
+    <div className='w-full flex flex-inline items-center justify-between bg-white/20 gap-4 mt-6'>
+      <div className='flex flex-inline justify-center items-center gap-x-6 ml-8'>
+        <img src={Logo} alt="Logo" className='w-[60px] sm:w-[86px] lg:w-[100px] xl:w-[120px]' />
+        <img src={Banners} alt="Banners" className='w-[120px] sm:w-[180px] lg:w-[280px] xl:w-[380px]' />
+      </div>
+      <div className='flex flex-inline justify-between gap-x-4 items-center mr-12'>
+        <h1>Home</h1>
+        <h1>Portfólio</h1>
+        <h1>Currículo</h1>
+        <h1>Contatos</h1>
+      </div>
     </div>
   )
 }
