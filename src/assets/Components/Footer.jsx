a2f657f48f469000a203fbb1369d6fe25bab4d1e
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2025-10-29 12:42:01 -0300

    Update Footer.jsx

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { PiLinkedinLogo } from "react-icons/pi";
+import { FiYoutube } from "react-icons/fi";
+import { FaArtstation } from "react-icons/fa";
+import { VscGithub } from "react-icons/vsc";
 
 const Footer = () => {
   return (
-    <div className='flex justify-center items-center w-full h-[80px] font-text bg-dark-gray dark:bg-primary/20 backdrop-blur-md'>
-      <h1 className='text-[45px]'>Footer</h1>
+    <div className='flex flex-col md:flex-row justify-between items-center w-full h-[110px] sm:h-[80px] lg:h-[80px] xl:h-[80px] font-text bg-dark-gray dark:bg-primary/20 backdrop-blur-md fixed bottom-0 left-0 px-14 pt-2 sm:pt-0 xl:pt-0 pb-6 sm:pb-0 xl:pb-0'>
+      <span className='text-center md:text-left'>
+        {new Date().getFullYear()} Feito por Estevam Jannuzzi &copy;. Todos os direitos reservados.
+      </span>
+        <div className='flex gap-4 mt-2 md:mt-0 md:ml-auto'>
+          <a href='https://www.linkedin.com/in/estevam-jannuzzi-217355bb/' target='_blank' rel='noopener noreferrer' className='cursor-pointer bg-transparent border-none p-0 hover:text-orange dark:hover:text-green'>
+            <PiLinkedinLogo size={24} />
+          </a>
+          <a href='https://www.youtube.com/@vfx_3D_world' target='_blank' rel='noopener noreferrer' className='cursor-pointer bg-transparent border-none p-0 hover:text-orange dark:hover:text-green'>
+            <FiYoutube size={24} />
+          </a>
+          <a href='https://www.artstation.com/estevamjannuzzi4' target='_blank' rel='noopener noreferrer' className='cursor-pointer bg-transparent border-none p-0 hover:text-orange dark:hover:text-green'>
+            <FaArtstation size={24} />
+          </a>
+          <a href='https://github.com/EstevamJannuzzi' target='_blank' rel='noopener noreferrer' className='cursor-pointer bg-transparent border-none p-0 hover:text-orange dark:hover:text-green'>
+            <VscGithub size={24} />
+          </a>
+      </div>
     </div>
   )
 }
