bca85fe5c9b94733d7527236072dcf7ad8f3ff36
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2025-10-30 10:02:38 -0300

    create dingbo

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,24 +1,32 @@
 import React from 'react'
 import DefaultScreen from '../assets/Components/DefaultScreen.jsx'
+import DingBo from '../assets/images/DingBo.1.webp'
 
 const Home = () => {
     return (
         <DefaultScreen className='z-30'>
             <div>
-                <h1 className="text-4xl font-bold text-orange dark:text-green mb-6">
+                <h1 className="text-4xl font-bold text-orange dark:text-green mb-12">
                     Bem-vindo ao Portfólio de <br /> <span className='xl:text-[45px] text-primary'>Estevam Jannuzzi</span>
                 </h1>
-                <p className="font-text text-xl text-dark-gray dark:text-light-gray max-w-3xl mx-auto">Designer 3D  Especialista em Visualização Digital  Domínio em Maya, ZBrush, Photoshop, KeyShot e After Effect
+            </div>
+            <div className='flex flex-col sm:flex-row lg:flex-row xl:flex-row items-center justify-center gap-6'>
+                <p className="font-text text-xl text-dark-gray dark:text-light-gray bg-white dark:bg-dark-gray rounded-lg p-4 w-[400px]">Designer 3D  Especialista em Visualização Digital  Domínio em Maya, ZBrush, Photoshop, KeyShot e After Effect
                 </p>
-                <p className="font-text text-xl text-dark-gray dark:text-light-gray max-w-3xl mx-auto">
+                <p className="font-text text-xl text-dark-gray dark:text-light-gray bg-white dark:bg-dark-gray rounded-lg p-4 w-[400px]">
                     Sou formado em Design Gráfico desde 2011, com mais de uma década de experiência em criação visual e modelagem 3D. Tenho domínio técnico em Maya, ZBrush, Photoshop, KeyShot e After Effects — ferramentas que utilizo para transformar ideias em imagens impactantes, seja em projetos de produto, animação, visualização arquitetônica ou motion design.
                 </p>
-                <p className="font-text text-xl text-dark-gray dark:text-light-gray max-w-3xl mx-auto">
+            </div>
+            <div className='flex flex-col sm:flex-row lg:flex-row xl:flex-row items-center justify-center gap-6'>
+                <p className="font-text text-xl text-dark-gray dark:text-light-gray bg-white dark:bg-dark-gray rounded-lg p-4 w-[400px]">
                     Além do design, sou entusiasta de tecnologia e programação, com foco em React JS e Python. Essa combinação me permite integrar arte e lógica, criando soluções visuais interativas e funcionais.
                 </p>
-                <p className="font-text text-xl text-dark-gray dark:text-light-gray max-w-3xl mx-auto">
+                <p className="font-text text-xl text-dark-gray dark:text-light-gray bg-white dark:bg-dark-gray rounded-lg p-4 w-[400px]">
                     Busco oportunidades em design 3D, tech art, visualização digital e projetos que valorizem criatividade, técnica e inovação. Vamos nos conectar?
                 </p>
+            </div>
+            <div>
+                <img src={DingBo} alt="DingBo" className='flex justify-center items-center w-[380px] sm:w-[500px] lg:w-[600px] xl:w-[800px]' />
             </div>
         </DefaultScreen>
     )
--- src/assets/Components/DefaultScreen.jsx
+++ src/assets/Components/DefaultScreen.jsx
@@ -6,7 +6,7 @@ const DefaultScreen = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col bg-light-gray dark:bg-black transition-colors">
       <Menu />
-      <main className="flex-grow pt-[72px]">
+      <main className="flex-grow pt-[72px] mb-30 sm:mb-24 lg:mb-24 xl:mb-20">
         <div className="container mx-auto pt-14 sm:pt-20 lg:pt-28 xl:pt-32 flex items-center justify-center h-full">
           <div className="text-center text-gray-900 dark:text-white">{children}</div>
         </div>
