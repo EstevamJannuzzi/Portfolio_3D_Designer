1a22e01a0149fbcb803457e25b772163b9d153ba
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2025-10-30 14:48:28 -0300

    imagem carnivor

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import DefaultScreen from '../assets/Components/DefaultScreen.jsx'
 import DingBo from '../assets/images/DingBo.1.webp'
+import Carnivor from '../assets/images/carnivor.1.webp'
 import BoxText from '../assets/Components/BoxText.jsx'
 import Title from '../assets/Components/Title.jsx'
+import { Car } from 'lucide-react'
 
 const Home = () => {
     return (
@@ -33,15 +35,21 @@ const Home = () => {
                     className="flex justify-center items-center w-[260px] sm:w-[500px] lg:w-[600px] xl:w-[900px] ml-22 sm:ml-0 lg:ml-0 xl:ml-0 -mt-50 z-10 relative"
                 />
             </div>
-
-            <div className='flex flex-col items-center justify-center gap-6 mb-12'>
+            <div className='flex flex-col items-center justify-center gap-6 mb-12 mt-22'>
+                <img
+                    src={Carnivor}
+                    alt="Carnivor"
+                    className="flex justify-center items-center w-[260px] sm:w-[500px] lg:w-[600px] xl:w-[900px] -ml-14 sm:ml-0 lg:-ml-20 xl:ml-0 z-10 relative"
+                />
                 <BoxText
-                    text="Sou formado em Design Gráfico desde 2011, com mais de uma década de experiência em criação visual e modelagem 3D. Tenho domínio técnico em Maya, ZBrush, Photoshop, KeyShot e After Effects — ferramentas que utilizo para transformar ideias em imagens impactantes, seja em projetos de produto, animação, visualização arquitetônica ou motion design."
+                    text="Sou formado em Design Gráfico desde 2011, com mais de uma década de experiência em criação visual e modelagem 3D. Tenho domínio técnico em Maya, ZBrush, Photoshop, KeyShot e Substance 3D Painter — ferramentas que utilizo para transformar ideias em imagens impactantes, seja em projetos de produto, animação, visualização arquitetônica ou motion design."
                     width="max-w-60 sm:max-w-60 lg:max-w-80 xl:max-w-90"
                     distance="z-20 relative"
-                    margin="mb-24 sm:mb-4 lg:mb-0 xl:mb-0 mr-30 sm:mr-70 lg:mr-90 xl:mr-100"
+                    margin="-mt-20 sm:-mt-80 lg:-mt-100 xl:-mt-126 ml-30 sm:ml-60 lg:ml-70 xl:ml-120"
                     size="text-[14px] sm:text-[18px] lg:text-[20px] xl:text-[22px]"
-                />
+                />   
+            </div>
+            <div className='flex flex-col items-center justify-center gap-6 mb-12'>
                 <BoxText
                     text="Além do design, sou entusiasta de tecnologia e programação, com foco em React JS e Python. Essa combinação me permite integrar arte e lógica, criando soluções visuais interativas e funcionais."
                     width="max-w-60 sm:max-w-60 lg:max-w-80 xl:max-w-90"
