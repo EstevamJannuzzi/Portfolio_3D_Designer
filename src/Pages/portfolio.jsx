e61294fd1a8bb7482aaac2373dd6e90456ff68c8
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2025-11-02 15:33:19 -0300

    tradução Portfolio

--- src/Pages/portfolio.jsx
+++ src/Pages/portfolio.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import DefaultScreen from '../assets/Components/DefaultScreen.jsx'
 import Title from '../assets/Components/Title.jsx'
+import { useTranslation } from 'react-i18next'
 
 const Portfolio = () => {
+  const { t } = useTranslation()
+
   return (
     <DefaultScreen className='z-30'>
       <div className='flex flex-col items-center justify-center gap-2 text-center'>
         <Title
-          text="Portfólio"
+          text={t("portfolio.title")}
           size="text-[20px] sm:text-[32px] lg:text-[36px] xl:text-[60px]"
         />
       </div>
