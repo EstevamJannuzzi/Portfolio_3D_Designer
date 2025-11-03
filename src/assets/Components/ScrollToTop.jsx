d5ae0a9ca08bf8edaa28a6a6ca0f3eea39c69b9b
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2025-11-03 10:27:55 -0300

    Update ScrollToTop.jsx

--- src/assets/Components/ScrollToTop.jsx
+++ src/assets/Components/ScrollToTop.jsx
@@ -35,7 +35,7 @@ const ScrollToTop = () => {
     <button
       onClick={scrollToTop}
       className={`fixed bottom-32 sm:bottom-20 lg:bottom-20 xl:bottom-20 right-2 sm:right-6 lg:right-12 xl:right-12 transition-opacity text-orange hover:text-green dark:text-green dark:hover:text-orange text-3xl 
-              ${isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}
+              ${isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none'} cursor-pointer z-50`}
     >
       <IoIosArrowDropup size={50} />
     </button>
