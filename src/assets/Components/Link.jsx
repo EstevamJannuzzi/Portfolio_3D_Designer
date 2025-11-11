bc94fbf3d5e904be37f6e94789678de7a2da9f97
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2025-11-11 08:40:53 -0300

    Update Link.jsx

--- src/assets/Components/Link.jsx
+++ src/assets/Components/Link.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Link = ({ text, link }) => {
     return (
-        <div className="flex flex-col items-center justify-center font-text text-dark-gray dark:text-light-gray text-[14px] sm:text-[18px] lg:text-[20px] xl:text-[22px] cursor-pointer">
+        <div className="flex flex-col items-center justify-center font-text text-orange dark:text-green hover:text-green dark:hover:text-orange text-[14px] sm:text-[18px] lg:text-[20px] xl:text-[22px] cursor-pointer">
             <a
                 href={link}
                 target='_blank'
