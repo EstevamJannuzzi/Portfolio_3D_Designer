cd27413fd5ab64818291c370d8a0ed0b48557a68
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2025-11-11 16:35:03 -0300

    Update Programação

--- src/Pages/programacao.jsx
+++ src/Pages/programacao.jsx
@@ -22,24 +22,41 @@ const Programacao = () => {
                 />
             </div>
 
-            <div className='flex flex-col justify-center items-center mt-6'>
-                <Image
-                    src='/tictactoe.webp'
-                    alt='Game Jogo da Velha'
-                    width='w-[160px] sm:w-[280px] lg:w-[340px] xl:w-[420px]'
-                    margin='mb-4'
-                />
-                <Link
-                    text={t("programming.text")}
-                    link='/Programs/TicTacToe_v2.exe'
-                    download={true}
-                />
+            <div className="relative flex justify-center items-center mt-8">
+                <div className="grid grid-cols-2 gap-6">
+                    <div className='flex flex-col justify-center items-center mt-6'>
+                        <Image
+                            src='/tictactoe.webp'
+                            alt='Game Jogo da Velha'
+                            width='w-[160px] sm:w-[280px] lg:w-[340px] xl:w-[420px]'
+                            margin='mb-4'
+                        />
+                        <Link
+                            text={t("programming.text")}
+                            link='/Programs/TicTacToe_v2.exe'
+                            download={true}
+                        />
+                    </div>
+                    <div className='flex flex-col justify-center items-center mt-6'>
+                        <Image
+                            src='/Apostas.webp'
+                            alt='Game Aposta'
+                            width='w-[160px] sm:w-[280px] lg:w-[340px] xl:w-[420px]'
+                            margin='mb-4'
+                        />
+                        <Link
+                            text={t("programming.text_A")}
+                            link='/Programs/Apostas.exe'
+                            download={true}
+                        />
+                    </div>
+                </div>
             </div>
 
             <div className="mt-8 flex justify-center relative z-20">
                 <BackButton to="/portfolio" />
             </div>
-        </DefaultScreen>
+        </DefaultScreen >
     )
 }
 
