ffa42df3348c2d3d9d2ee32d0f577fe48edd42ec
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2025-11-12 09:15:51 -0300

    Update BoxContent.jsx

--- src/assets/Components/BoxContent.jsx
+++ src/assets/Components/BoxContent.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { PiWindowsLogoBold } from "react-icons/pi"
 
-const BoxContent = ({ text, icon, link, width='w-[100px] sm:w-[150px] lg:w-[200px] xl:w-[250px]', target = '_blank', download = false }) => {
+const BoxContent = ({
+    text,
+    icon,
+    link,
+    width = 'w-[100px] sm:w-[150px] lg:w-[200px] xl:w-[250px]',
+    target = '_blank',
+    download = false
+}) => {
     return (
         <a
             href={link}
@@ -10,7 +18,14 @@ const BoxContent = ({ text, icon, link, width='w-[100px] sm:w-[150px] lg:w-[200p
             className={`${width} flex flex-col items-center justify-center font-text text-dark-gray dark:text-light-gray bg-white/65 dark:bg-dark-gray/85 rounded-lg p-2 text-[14px] sm:text-[18px] lg:text-[20px] xl:text-[22px] border-b-8 border-orange hover:border-primary dark:border-green dark:hover:border-orange hover:scale-105 transition-transform duration-300 cursor-pointer`}
         >
             <div className="flex flex-col items-center gap-2 text-orange hover:text-primary dark:text-green dark:hover:text-orange text-[16px] sm:text-[18px] lg:text-[20px]">
-                {icon && <span>{icon}</span>}
+                {/* Se for download, mostra o ícone do Windows */}
+                {download ? (
+                    <span>
+                        <PiWindowsLogoBold className="text-[24px] sm:text-[28px] lg:text-[32px]" />
+                    </span>
+                ) : (
+                    icon && <span>{icon}</span>
+                )}
                 <span className="font-text text-[14px] sm:text-[22px] lg:text-[28px] xl:text-[32px]">
                     {text}
                 </span>
