c64886b38583868636a210b37dd71ddf1a6309ef
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2025-11-12 07:49:06 -0300

    Update Link.jsx

--- src/assets/Components/Link.jsx
+++ src/assets/Components/Link.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Link = ({ text, link, download = false }) => {
     return (
-        <div className="flex flex-col items-center justify-center font-text text-orange dark:text-green hover:text-primary dark:hover:text-orange text-[14px] sm:text-[18px] lg:text-[20px] xl:text-[22px] cursor-pointer">
+        <div className="flex flex-col items-center justify-center font-text text-orange dark:text-green hover:text-primary dark:hover:text-orange text-[14px] sm:text-[18px] lg:text-[20px] xl:text-[22px] bg-white/65 dark:bg-dark-gray/85 rounded-lg p-2 border-b-8 border-orange hover:border-primary dark:border-green dark:hover:border-orange hover:scale-105 transition-transform duration-300 cursor-pointer">
             <a
                 href={link}
                 {...(download
