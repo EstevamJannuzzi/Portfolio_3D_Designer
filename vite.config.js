a9d2c934e76cec290a6c0a90dd0e05447e29e4b7
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2025-12-03 09:57:21 -0300

    Update to site

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Portfolio_3D_Designer/', // 👈 ajuste obrigatório para GitHub Pages
   plugins: [
     react(),
     tailwindcss()
