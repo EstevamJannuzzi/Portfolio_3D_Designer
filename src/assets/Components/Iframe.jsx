25d16e9e33f83a44fbcfb3427177572bc28459c3
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2025-12-03 15:13:01 -0300

    Update Iframe.jsx

--- src/assets/Components/Iframe.jsx
+++ src/assets/Components/Iframe.jsx
@@ -7,8 +7,8 @@ const Iframe = ({
   referrerPolicy = 'strict-origin-when-cross-origin',
   allowFullScreen = true,
   wrapperClass = 'relative flex justify-center items-center w-full',
-  width = 'w-[380px] sm:w-[640px] md:w-[640px] lg:w-[750px] xl:w-[500px]',
-  height = 'h-[164px] sm:h-[360px] md:h-[360px] lg:h-[420px] xl:h-[280px]',
+  width = 'w-[380px] sm:w-[640px] md:w-[640px] lg:w-[750px] xl:w-[360px]',
+  height = 'h-[164px] sm:h-[360px] md:h-[360px] lg:h-[420px] xl:h-[180px]',
   className = 'rounded-xl z-10',
 }) => {
   return (
