77afb4773d28baee5500e617a4a4adc34101826e
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2026-01-15 14:16:11 -0300

    Green color

--- src/assets/Components/BoxContent.jsx
+++ src/assets/Components/BoxContent.jsx
@@ -15,9 +15,9 @@ const BoxContent = ({
             {...(download
                 ? { download: true }
                 : { target: target, rel: 'noopener noreferrer' })}
-            className={`${width} flex flex-col items-center justify-center font-text text-dark-gray dark:text-light-gray bg-white/65 dark:bg-dark-gray/85 rounded-lg p-2 text-[14px] sm:text-[18px] lg:text-[20px] xl:text-[22px] border-b-8 border-orange hover:border-primary dark:border-green dark:hover:border-orange hover:scale-105 transition-transform duration-300 cursor-pointer`}
+            className={`${width} flex flex-col items-center justify-center font-text text-dark-gray dark:text-light-gray bg-white/65 dark:bg-dark-gray/85 rounded-lg p-2 text-[14px] sm:text-[18px] lg:text-[20px] xl:text-[22px] border-b-8 border-orange hover:border-green dark:border-green dark:hover:border-orange hover:scale-105 transition-transform duration-300 cursor-pointer`}
         >
-            <div className="flex flex-col items-center gap-2 text-orange hover:text-primary dark:text-green dark:hover:text-orange text-[16px] sm:text-[18px] lg:text-[20px]">
+            <div className="flex flex-col items-center gap-2 text-orange hover:text-green dark:text-green dark:hover:text-orange text-[16px] sm:text-[18px] lg:text-[20px]">
                 {/* Se for download, mostra o ícone do Windows */}
                 {download ? (
                     <span>
