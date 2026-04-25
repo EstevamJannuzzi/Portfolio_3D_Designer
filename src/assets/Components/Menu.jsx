fb91fcfa7a7b52d541b985db51b93b39edcb4b1d
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2026-04-25 11:36:34 -0300

    Changes

--- src/assets/Components/Menu.jsx
+++ src/assets/Components/Menu.jsx
@@ -64,7 +64,7 @@ export default function MenuBar() {
   );
 
   return (
-    <header className="w-full flex items-center justify-between bg-dark-gray/75 dark:bg-primary/20 backdrop-blur-md sm:h-[60px] lg:h-[140px] gap-4 py-3 px-6 fixed top-0 left-0 z-50 shadow-sm">
+    <header className="w-full flex items-center justify-between bg-dark-gray dark:bg-primary sm:h-[60px] lg:h-[140px] gap-4 py-3 px-6 fixed top-0 left-0 z-50 shadow-sm">
 
       {/* LOGO */}
       <div className="flex items-center gap-x-4 xl:gap-x-6 ml-0 sm:ml-90 lg:ml-6 xl:ml-0">
