7d7323155290f1cd20144af7888cc9b09f1b7114
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2026-07-28 12:18:02 -0300

    modify BG

--- src/assets/Components/DefaultScreen.jsx
+++ src/assets/Components/DefaultScreen.jsx
@@ -5,7 +5,7 @@ import ScrollToTop from './ScrollToTop.jsx'
 
 const DefaultScreen = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col bg-light-gray dark:bg-secondary transition-colors">
+    <div className="min-h-screen flex flex-col bg-[url(/bgLight.webp)] bg-center bg-cover dark:bg-[url(/bgDark.webp)] dark:bg-center dark:bg-cover transition-colors">
       <Menu />
 
       <main className="flex-grow pt-[72px] mb-30 sm:mb-24 lg:mb-24 xl:mb-20">
