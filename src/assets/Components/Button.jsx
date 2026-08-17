67883651bab7d9321225ce8128ca25e887e6020b
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2026-08-17 11:54:57 -0300

    Adjust Name

--- src/assets/Components/Button.jsx
+++ src/assets/Components/Button.jsx
@@ -13,16 +13,16 @@ const Button = ({
             onClick={onClick}
             className={`${width} flex flex-col items-center justify-center font-text text-dark-gray dark:text-light-gray bg-white/65 dark:bg-dark-gray/85 rounded-lg p-2 text-[14px] sm:text-[18px] lg:text-[20px] xl:text-[22px] border-b-8 border-sky-blue hover:border-blue-iris dark:border-blue-iris dark:hover:border-sky-blue hover:scale-105 transition-transform duration-300 cursor-pointer`}
         >
-            <div className="flex flex-col items-center gap-2 text-sky-blue hover:text-blue-iris dark:text-blue-iris dark:hover:text-sky-blue text-[16px] sm:text-[18px] lg:text-[20px]">
+            <div className="flex flex-col items-center gap-1 text-sky-blue hover:text-blue-iris dark:text-blue-iris dark:hover:text-sky-blue">
                 {/* Se for download, mostra o ícone do Windows */}
                 {download ? (
                     <span>
-                        <PiWindowsLogoBold className="text-[24px] sm:text-[28px] lg:text-[32px]" />
+                        <PiWindowsLogoBold className="text-[20px] lg:text-[24px]" />
                     </span>
                 ) : (
                     icon && <span>{icon}</span>
                 )}
-                <span className="font-text text-[12px] sm:text-[14px] xl:text-[16px]">
+                <span className="font-text text-[12px] xl:text-[16px]">
                     {text}
                 </span>
             </div>
